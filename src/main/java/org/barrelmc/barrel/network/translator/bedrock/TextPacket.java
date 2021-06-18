0ccc02eb6fc1266365ef4dadfc365d885bdf05e9
brokiem <milyhqvideo@gmail.com>
2021-06-18 20:26:44 +0800

    send text packet with correct type

--- src/main/java/org/barrelmc/barrel/network/translator/bedrock/TextPacket.java
+++ src/main/java/org/barrelmc/barrel/network/translator/bedrock/TextPacket.java
@@ -1,5 +1,7 @@
 package org.barrelmc.barrel.network.translator.bedrock;
 
+import com.github.steveice10.mc.protocol.data.game.MessageType;
+import com.github.steveice10.mc.protocol.packet.ingame.server.ServerChatPacket;
 import com.nukkitx.protocol.bedrock.BedrockPacket;
 import org.barrelmc.barrel.network.translator.interfaces.BedrockPacketTranslator;
 import org.barrelmc.barrel.player.Player;
@@ -10,6 +12,20 @@ public class TextPacket implements BedrockPacketTranslator {
     public void translate(BedrockPacket pk, Player player) {
         com.nukkitx.protocol.bedrock.packet.TextPacket packet = (com.nukkitx.protocol.bedrock.packet.TextPacket) pk;
 
-        player.sendMessage(packet.getMessage());
+        switch (packet.getType()) {
+            case TIP:
+            case POPUP: {
+                player.getJavaSession().send(new ServerChatPacket(packet.getMessage(), MessageType.NOTIFICATION));
+                break;
+            }
+            case SYSTEM: {
+                player.getJavaSession().send(new ServerChatPacket(packet.getMessage(), MessageType.SYSTEM));
+                break;
+            }
+            default: {
+                player.sendMessage(packet.getMessage());
+                break;
+            }
+        }
     }
 }
