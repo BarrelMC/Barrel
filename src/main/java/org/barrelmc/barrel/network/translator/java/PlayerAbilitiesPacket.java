e380c13a9719ee9d99ef0ac306876da3396b0de7
brokiem <milyhqvideo@gmail.com>
2023-04-09 13:17:21 +0800

    fix: Invalid AdventureSettingsPacket

--- src/main/java/org/barrelmc/barrel/network/translator/java/PlayerAbilitiesPacket.java
+++ src/main/java/org/barrelmc/barrel/network/translator/java/PlayerAbilitiesPacket.java
@@ -4,8 +4,6 @@ import com.github.steveice10.mc.protocol.codec.MinecraftPacket;
 import com.github.steveice10.mc.protocol.packet.ingame.serverbound.player.ServerboundPlayerAbilitiesPacket;
 import com.nukkitx.protocol.bedrock.data.Ability;
 import com.nukkitx.protocol.bedrock.data.AbilityType;
-import com.nukkitx.protocol.bedrock.data.AdventureSetting;
-import com.nukkitx.protocol.bedrock.packet.AdventureSettingsPacket;
 import com.nukkitx.protocol.bedrock.packet.RequestAbilityPacket;
 import org.barrelmc.barrel.network.translator.interfaces.JavaPacketTranslator;
 import org.barrelmc.barrel.player.Player;
@@ -15,12 +13,6 @@ public class PlayerAbilitiesPacket implements JavaPacketTranslator {
     public void translate(MinecraftPacket pk, Player player) {
         ServerboundPlayerAbilitiesPacket packet = (ServerboundPlayerAbilitiesPacket) pk;
 
-        if (packet.isFlying()) {
-            AdventureSettingsPacket adventureSettingsPacket = new AdventureSettingsPacket();
-            adventureSettingsPacket.setUniqueEntityId(player.getRuntimeEntityId());
-            adventureSettingsPacket.getSettings().add(AdventureSetting.FLYING);
-            player.getBedrockClient().getSession().sendPacket(adventureSettingsPacket);
-        }
         RequestAbilityPacket requestAbilityPacket = new RequestAbilityPacket();
         requestAbilityPacket.setAbility(Ability.FLYING);
         requestAbilityPacket.setType(AbilityType.BOOLEAN);
